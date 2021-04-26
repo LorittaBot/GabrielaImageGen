97df1b1ef4f81fd3ac03e73208a6901d653da3c6
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-26 12:33:54 -0300

    Add Docker image
    Will this work? heh, probably not

---
+++ docker/gabriela-image-server/files/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env ash
+
+# Because the config file is on this folder, we need to cd to there first
+cd /home/gabriela-image-server/
+java -Xmx1G -jar /home/gabriela-image-server/image-generation-server-fat-1.0-SNAPSHOT.jar
